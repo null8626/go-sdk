a01282340e11068dc556f0e423c291f246b16b7b
null <60427892+null8626@users.noreply.github.com>
2026-03-11 20:06:28 +0700

    refactor: add BaseURL to Sprintf arguments

--- widget.go
+++ widget.go
@@ -4,20 +4,20 @@ import "fmt"
 
 // LargeWidget is a function that generates a large widget URL.
 func LargeWidget(platform Platform, projectType ProjectType, id string) string {
-	return fmt.Sprintf(BaseURL+"/widgets/large/%s/%s/%s", string(platform), string(projectType), id)
+	return fmt.Sprintf("%s/widgets/large/%s/%s/%s", BaseURL, string(platform), string(projectType), id)
 }
 
 // VotesWidget is a function that generates a small widget URL for displaying votes.
 func VotesWidget(platform Platform, projectType ProjectType, id string) string {
-	return fmt.Sprintf(BaseURL+"/widgets/small/votes/%s/%s/%s", string(platform), string(projectType), id)
+	return fmt.Sprintf("%s/widgets/small/votes/%s/%s/%s", BaseURL, string(platform), string(projectType), id)
 }
 
 // OwnerWidget is a function that generates a small widget URL for displaying a project's owner.
 func OwnerWidget(platform Platform, projectType ProjectType, id string) string {
-	return fmt.Sprintf(BaseURL+"/widgets/small/owner/%s/%s/%s", string(platform), string(projectType), id)
+	return fmt.Sprintf("%s/widgets/small/owner/%s/%s/%s", BaseURL, string(platform), string(projectType), id)
 }
 
 // SocialWidget is a function that generates a small widget URL for displaying social stats.
 func SocialWidget(platform Platform, projectType ProjectType, id string) string {
-	return fmt.Sprintf(BaseURL+"/widgets/small/social/%s/%s/%s", string(platform), string(projectType), id)
+	return fmt.Sprintf("%s/widgets/small/social/%s/%s/%s", BaseURL, string(platform), string(projectType), id)
 }
